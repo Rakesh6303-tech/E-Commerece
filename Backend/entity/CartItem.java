af2278cc3217690e7099579c5dba5e3dda94a91a
Rakesh <dr8452799@gmail.com>
2025-07-16 08:14:51 +0200

    Create CartItem.java

---
+++ Backend/entity/CartItem.java
@@ -0,0 +1,9 @@
+@Entity
+public class CartItem {
+    @Id @GeneratedValue private Long id;
+
+    @ManyToOne private User user;
+    @ManyToOne private Product product;
+
+    private int quantity;
+}
