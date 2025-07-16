dd9b9ade8c904ffacbbe224b115c678ca0a9a2cc
Rakesh <dr8452799@gmail.com>
2025-07-16 08:13:27 +0200

    Create user.java

---
+++ Backend/entity/user.java
@@ -0,0 +1,8 @@
+@Entity
+public class User {
+    @Id @GeneratedValue private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role; // "ADMIN" or "CUSTOMER"
+}
